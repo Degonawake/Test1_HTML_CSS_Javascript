d8a98ed76fbc1fda3fd1b707c027394ed6d1c833
Degonawake <diegoisgonzales@gmail.com>
2024-01-15 08:08:42 -0600

    TnewHttpCSSJavaScript

--- WebApplication3/wwwroot/js/site.js
+++ WebApplication3/wwwroot/js/site.js
@@ -1,16 +1,37 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your JavaScript code.
-function calculate()
+function  calculate()
 {
-    beginYear = document.getElementById("hireDate").value;
-    var date1 = new Date(beginYear)
-    var date2 = new Date(Date.now().toString())
+    
     var beginYear;
+    beginYear = document.getElementById("hireDate").value;
+    var date1 = new Date(beginYear);
+    var date2 = new Date(Date.now());
 
-    console.log(date1);
-    console.log(Date.now().toString());
-    var diffDays = date2.getDate() - date1.getDate();
-    console.log(diffDays);
+
+    if (verify() === 1)
+    {
+        alert("Name or hireDate cannot be empty")
+        document.getElementById("AgeResult").innerHTML = "";
+    }
+    else
+    { 
+    var diffYears = date2.getFullYear() - date1.getFullYear();
+    console.log(diffYears);
+
+    document.getElementById("AgeResult").innerHTML = "Employement years: "+ diffYears;
+    }
+}
+
+function verify()
+{
+    var name = document.getElementById("name");
+    var date = document.getElementById("hireDate");
+
+    if (name.value === "" || hireDate.value=== "")
+    {
+        return 1;
+    }
+    return 0;
 }
